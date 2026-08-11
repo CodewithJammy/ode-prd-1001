e7ee6cf3787df78c75e87b8ff0ffebac550eb236
jammyIT <hsamsed@gmail.com>
2026-08-11 11:04:42 +0530

    Update app.py

--- app.py
+++ app.py
@@ -12,12 +12,9 @@ app.secret_key = os.getenv("FLASK_SECRET_KEY")
 app.register_blueprint(upload_bp)
 app.register_blueprint(demotest_bp)
 
-@app.route('/')
-def index():
-    user_id = session.get('user_id')
-    return render_template('index.html', is_logged_in=bool(user_id))
-@app.route('/search', methods=['GET', 'POST'])
-
+@app.route("/")
+def home():
+    return render_template("home.html")
 
 if __name__ == "__main__":
     with app.app_context():
