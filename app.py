6f8e71cf18b7596de1d05ad86c8f99e669861621
jammyIT <hsamsed@gmail.com>
2026-08-12 11:34:10 +0530

    Update app.py

--- app.py
+++ app.py
@@ -5,12 +5,17 @@ from flask_cors import CORS
 from flask_mail import Mail, Message
 from routes.upload import upload_bp
 from routes.demo_test import demotest_bp
+from route.auth import auth_bp
+from route.user import user_bp
+
 
 
 app = Flask(__name__)
 app.secret_key = os.getenv("FLASK_SECRET_KEY")
 app.register_blueprint(upload_bp)
 app.register_blueprint(demotest_bp)
+app.register_blueprint(auth_bp)
+app.register_blueprint(user_bp)
 
 @app.route("/")
 def home():
