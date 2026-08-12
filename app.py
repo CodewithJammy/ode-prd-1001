8fbbdbfeb8dade95300029d7a2698ac63c7cacfd
jammyIT <hsamsed@gmail.com>
2026-08-12 13:06:42 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,16 +1,26 @@
-from flask import Flask,  Response, render_template, redirect ,request, session
-import logging
+
 import os
+import logging
 from flask_cors import CORS
 from flask_mail import Mail, Message
-from routes.upload import upload_bp
-from routes.demo_test import demotest_bp
+from werkzeug.middleware.proxy_fix import ProxyFix
+from flask import Flask,  Response, render_template, redirect ,request, session
+
 from routes.auth import auth_bp
 from routes.user import user_bp
+from routes.upload import upload_bp
+from routes.demo_test import demotest_bp
+
 
 
 
 app = Flask(__name__)
+
+app.wsgi_app = ProxyFix(
+    app.wsgi_app,
+    x_proto=1,
+    x_host=1
+)
 app.secret_key = os.getenv("FLASK_SECRET_KEY")
 app.register_blueprint(upload_bp)
 app.register_blueprint(demotest_bp)
